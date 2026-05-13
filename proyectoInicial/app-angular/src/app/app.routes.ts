317f72c0efde7942b6d38d1d1c5be0076066cd03
MarcosBayget <marcosbayget@gmail.com>
2026-05-13 16:39:17 -0300

    formulario y tabla de alumnos

--- proyectoInicial/app-angular/src/app/app.routes.ts
+++ proyectoInicial/app-angular/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './views/home/home';
 import { Formulario } from './components/formulario/formulario';
 import { Dashboard } from './views/dashboard/dashboard';
+import { TableComponent } from './components/table/table';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'formulario', component: Formulario },
-  {path: 'dashboard', component : Dashboard}
+  { path: 'dashboard', component: Dashboard },
+  { path: 'alumnos', component: TableComponent },
 ];     
--- proyectoInicial/app-angular/src/app/components/formulario/formulario.ts
+++ proyectoInicial/app-angular/src/app/components/formulario/formulario.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, FormGroup } from '@angular/forms';
+
+import { z } from 'zod';
+
+const userSchema = z.object({
+  nombre: z.string().min(3, 'Nombre inválido'),
+  apellido: z.string().min(3, 'Apellido inválido'),
+  email: z.email('Email inválido'),
+  dni: z.string().regex(/^\d{8}$/, 'DNI inválido'),
+});
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
 export class Formulario {
+  errors: Record<string, string[]> = {};
+
+  // Declaramos el evento de salida
+  cerrar = output<void>();
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: [''],
+      apellido: [''],
+      email: [''],
+      dni: [''],
+    });
+  }
+
+  onCerrar() {
+    this.cerrar.emit();
+  }
+
+  onSubmit() {
+    const result = userSchema.safeParse(this.form.value);
+
+    if (!result.success) {
+      this.errors = result.error.flatten().fieldErrors;
+
+      return;
+    }
+
+    this.errors = {};
+
+    console.log('Formulario válido');
+    console.log(result.data);
+    this.onCerrar();
+  }
+
 
 }
