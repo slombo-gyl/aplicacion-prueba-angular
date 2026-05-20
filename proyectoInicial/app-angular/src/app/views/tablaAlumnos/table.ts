95cdf854439f497272dd37cf3ddb37a46806f3c7
DanielPalli <feerhh91@gmail.com>
2026-05-20 16:51:41 -0300

    refactor: los alumnos ahora se cargan desde el servicio

--- proyectoInicial/app-angular/src/app/views/tablaAlumnos/table.ts
+++ proyectoInicial/app-angular/src/app/views/tablaAlumnos/table.ts
@@ -1,24 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { TableModule } from 'primeng/table';
-import { Router, RouterLink } from "@angular/router";
+import { Router } from "@angular/router";
 import { Formulario } from '../../components/formulario/formulario';
+import { AlumnoService } from '../../services/alumnos.service';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  // Agregar los imports de este componente
   imports: [TableModule, Formulario],
   templateUrl: './table.html',
   styleUrl: './table.css'
 })
 export class TableComponent {
-  constructor(private router: Router) { }
+  private router = inject(Router);
+  alumnoService = inject(AlumnoService);
+
+  alumnos = computed(() => this.alumnoService.alumnos());
 
   navigateToHome() {
     this.router.navigate([''])
   }
 
-  // Estado del modal (equivalente a useState en React)
   mostrarFormulario = signal(false);
 
   abrirFormulario() {
@@ -28,11 +30,4 @@ export class TableComponent {
   cerrarFormulario() {
     this.mostrarFormulario.set(false);
   }
-
-  alumnos = [
-    { nombre: 'Juan Pérez', dni: '12345678', email: 'juan@example.com', notas: 8 },
-    { nombre: 'María García', dni: '87654321', email: 'maria@example.com', notas: 9 },
-    { nombre: 'Carlos López', dni: '11223344', email: 'carlos@example.com', notas: 6 },
-    { nombre: 'Ana Martínez', dni: '44332211', email: 'ana@example.com', notas: 10 }
-  ];
 }
