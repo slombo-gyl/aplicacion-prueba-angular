b9fd8e835103c706b7112f6d72823df99c0c9c14
DanielPalli <feerhh91@gmail.com>
2026-05-21 12:33:48 -0300

    feat: detalle alumno se agrego la interaccion con el formulario notas

--- proyectoInicial/app-angular/src/app/components/detalle-alumno/detalle-alumno.ts
+++ proyectoInicial/app-angular/src/app/components/detalle-alumno/detalle-alumno.ts
@@ -1,20 +1,44 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, inject, input, output, signal } from '@angular/core';
 import { AlumnoModel } from '../../interfaces/models/alumno.model';
 import { TableModule } from 'primeng/table';
+import { AlumnosService } from '../../services/alumnos.service';
+import { MateriaModel } from '../../interfaces/models/materia.model';
+import { FormularioNotas } from '../formulario-notas/formulario-notas';
 
 @Component({
   selector: 'app-detalle-alumno',
-  imports: [TableModule],
+  imports: [TableModule, FormularioNotas],
   templateUrl: './detalle-alumno.html',
   styleUrl: './detalle-alumno.css',
 })
 export class DetalleAlumno {
+  alumnosService = inject(AlumnosService);
   alumno = input.required<AlumnoModel>();
   cerrar = output<void>();
 
-  materiasMock = signal([
-    { nombre: 'Diseño', calificacion: 5 }
+  materias = signal<MateriaModel[]>([
+    { id: 1, nombreMateria: 'Diseño'}
   ]);
+
+  mostrarFormularioNotas = signal(false);
+  materiaSeleccionada = signal<MateriaModel | null>(null);
+
+  obtenerNota(materiaId: number): number | null {
+    const existePuntaje = this.alumnosService.puntajes().find(
+      puntaje => puntaje?.alumnoId === this.alumno().id && puntaje.materiaId === materiaId
+    );
+    return existePuntaje? existePuntaje.valor : null;
+  }
+
+  cargarNota(materia: MateriaModel) {
+    this.materiaSeleccionada.set(materia);
+    this.mostrarFormularioNotas.set(true);
+  }
+
+  cerrarFormularioNotas() {
+    this.mostrarFormularioNotas.set(false);
+    this.materiaSeleccionada.set(null);
+  }
 
   onCerrar() {
     this.cerrar.emit();
