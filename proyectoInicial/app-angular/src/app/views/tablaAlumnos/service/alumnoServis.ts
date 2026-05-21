4a41b2609e7ec8c31212c4afeee58bfc5152de3a
Leonardo L. Scuccimarra <dllopez@gylgroup.com>
2026-05-21 16:35:17 -0300

    DEV / 21 05 Actualizo métodos de table.ts y otras funciones de llamadas al backend

--- proyectoInicial/app-angular/src/app/views/tablaAlumnos/service/alumnoServis.ts
+++ proyectoInicial/app-angular/src/app/views/tablaAlumnos/service/alumnoServis.ts
@@ -1,32 +1,3 @@
-/*import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Alumno } from '../../../../interfaces/alumno.interfaces';
-
-
-
-@Injectable({
-    providedIn: 'root'
-})
-
-export class AlumnoService {
-    private BASE_URL = 'http://localhost:8080'
-
-    constructor(private http: HttpClient) { }
-
-    getAlumnos(): Observable<Alumno[]> {
-        return this.http.get<Alumno[]>(`${this.BASE_URL}/api/students`);
-    }
-
-    getAlumnoByID(id: number): Observable<Alumno>{
-        return this.http.get<Alumno>(`${this.BASE_URL}/api/students/${id}`);
-    }
-
-    crearAlumno(nuevoAlumno: Alumno): Observable<Alumno> {
-        return this.http.post<Alumno>(`${this.BASE_URL}/alumno`, nuevoAlumno);
-    }
-}*/
-
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -37,19 +8,21 @@ import { Alumno } from '../../../../interfaces/alumno.interfaces';
 })
 export class AlumnoService {
   private http = inject(HttpClient);
-  
-  // Cambia esto por la URL exacta de tu endpoint "listarEstudiantes"
-  private apiUrl = 'http://localhost:8080/api/students'; 
+    private apiUrl = 'http://localhost:8080/api/students'; 
 
   getAlumnos(): Observable<Alumno[]> {
     return this.http.get<Alumno[]>(this.apiUrl);
   }
 
   getAlumnoByID(id: number): Observable<Alumno> {
-    return this.http.get<Alumno>(`${this.apiUrl}/${id}`); // Ajusta si tu backend usa otra ruta para el ID
+    return this.http.get<Alumno>(`${this.apiUrl}/${id}`);
   }
 
   crearAlumno(nuevoAlumno: Alumno): Observable<Alumno> {
         return this.http.post<Alumno>(`${this.apiUrl}`, nuevoAlumno);
     }
+
+  deleteAlumnoLogico(id: number): Observable<Alumno> {
+    return this.http.delete<Alumno>(`${this.apiUrl}/${id}`);
+  }
 }
