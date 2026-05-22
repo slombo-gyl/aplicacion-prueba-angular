6ea9e38a3ec11ed988231016d7ce50638f362efa
DanielPalli <feerhh91@gmail.com>
2026-05-22 17:13:32 -0300

    refactor: nombre puntaje

--- proyectoInicial/app-angular/src/app/components/detalle-alumno/detalle-alumno.ts
+++ proyectoInicial/app-angular/src/app/components/detalle-alumno/detalle-alumno.ts
@@ -22,7 +22,7 @@ export class DetalleAlumno implements OnInit {
 
   ngOnInit() {
     this.materiasService.getMaterias().subscribe();
-    this.alumnosService.getPuntajes().subscribe();
+    this.alumnosService.getPuntajesList().subscribe();
   }
 
   mostrarFormularioNotas = signal(false);
